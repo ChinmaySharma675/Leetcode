f6ad33e503d66c1c1a93392b1a259cd7e9d53b81
ChinmaySharma675 <chinmay.sharma675@gmail.com>
2026-03-07 22:35:17 +0530

    Create 1585-TheKthFactorOfN

---
+++ 1585-TheKthFactorOfN/1585-TheKthFactorOfN.java
@@ -0,0 +1,16 @@
+// Last updated: 3/7/2026, 10:35:17 PM
+class Solution {
+    public int kthFactor(int n, int k) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                count++; // counts the kth factor starting from k=1 means 1st factor .
+                if(count==k){
+                    return i;
+                }
+            }
+        } 
+    return -1;
+        
+    }
+}
