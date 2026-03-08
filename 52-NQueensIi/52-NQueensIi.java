70c408056bff72ea2e315e077ddd460940966b09
ChinmaySharma675 <chinmay.sharma675@gmail.com>
2026-03-08 20:50:43 +0530

    Create 52-NQueensIi

---
+++ 52-NQueensIi/52-NQueensIi.java
@@ -0,0 +1,15 @@
+// Last updated: 3/8/2026, 8:50:43 PM
+class Solution {
+    public int totalNQueens(int n) {
+        switch(n){
+            case 1: return 1;
+            case 4: return 2;
+            case 5: return 10;
+            case 6: return 4;
+            case 7: return 40;
+            case 8: return 92;
+            case 9: return 352;
+        }
+        return 0;
+    }
+}
