9d8f2178508f6940c0facf000b8081349844f9b3
ChinmaySharma675 <chinmay.sharma675@gmail.com>
2026-08-09 18:55:17 +0530

    Create 1256-RankTransformOfAnArray

---
+++ 1256-RankTransformOfAnArray/1256-RankTransformOfAnArray.java
@@ -0,0 +1,21 @@
+// Last updated: 8/9/2026, 6:55:17 PM
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] sorted=arr.clone();
+        Arrays.sort(sorted);
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        int rank=1;
+        for(int num:sorted){
+            if(!map.containsKey(num)){
+                map.put(num,rank);
+                rank++;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            arr[i]=map.get(arr[i]);
+        }
+        return arr;
+    }
+}
