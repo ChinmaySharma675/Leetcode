5f7c8091852350c8d8412d4b75f8c6026a7f4267
ChinmaySharma675 <chinmay.sharma675@gmail.com>
2026-08-09 18:57:17 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.java
@@ -0,0 +1,19 @@
+// Last updated: 8/9/2026, 6:57:17 PM
+class Solution {
+    public int singleNumber(int[] nums) {
+      int result=0;
+      for(int i=0;i<32;i++){
+        int sum=0;
+        for(int num:nums){
+            if(((num>>i) & 1)==1) {
+                sum++;
+            }
+        }
+        if(sum%3 !=0){
+            result |= (1<<i);
+        }
+      }
+      return result;
+        
+    }
+}
