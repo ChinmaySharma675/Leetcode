3efdc3ecf07eacdf1195341374da6df99ba10e2c
ChinmaySharma675 <chinmay.sharma675@gmail.com>
2026-09-23 13:29:24 +0530

    Create 3010-DivideAnArrayIntoSubarraysWithMinimumCostI

---
+++ 3010-DivideAnArrayIntoSubarraysWithMinimumCostI/3010-DivideAnArrayIntoSubarraysWithMinimumCostI.java
@@ -0,0 +1,22 @@
+// Last updated: 9/23/2026, 1:29:23 PM
+class Solution {
+    public int minimumCost(int[] nums) {
+    int min1=51;
+    int min2=51;
+    int k=0;
+    for(int i=1;i<nums.length;i++){
+        if(nums[i]<=min1){
+            min1=nums[i];
+            k=i;
+
+        }
+    }
+      for(int i=1;i<nums.length;i++){
+        if(nums[i]<=min2 && i!=k){
+            min2=nums[i];
+        }
+    }
+    return nums[0]+min1+min2;
+        
+    }
+}
