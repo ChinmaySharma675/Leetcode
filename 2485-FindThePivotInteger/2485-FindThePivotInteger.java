214d04d40b143b6de5af80b11fc5cbf203041eae
ChinmaySharma675 <chinmay.sharma675@gmail.com>
2026-09-23 13:30:18 +0530

    Create 2485-FindThePivotInteger

---
+++ 2485-FindThePivotInteger/2485-FindThePivotInteger.java
@@ -0,0 +1,18 @@
+// Last updated: 9/23/2026, 1:30:17 PM
+class Solution {
+    public int pivotInteger(int n) {
+        int sum2=n*(n+1)/2; //total sum till number n.
+       
+        for(int i=1;i<=n;i++){
+            
+            int sum1=i*(i+1)/2; //sum1= sum till i so that we can check if i is pivot or not.
+            if(sum1== (sum2-sum1+i)){ //sum2-sum1 +i=right side sum including pivot no. 
+                return i;
+            }
+
+        }
+       
+        return -1;
+        
+    }
+}
