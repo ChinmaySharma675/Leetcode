96f6e8bd856e0d3c7c82a797ca4aaed2dfc62319
ChinmaySharma675 <chinmay.sharma675@gmail.com>
2026-09-23 13:29:21 +0530

    Create 3024-TypeOfTriangle

---
+++ 3024-TypeOfTriangle/3024-TypeOfTriangle.java
@@ -0,0 +1,15 @@
+// Last updated: 9/23/2026, 1:29:20 PM
+class Solution {
+    public String triangleType(int[] nums) {
+        if((nums[0]+nums[1]>nums[2]) && (nums[0]+nums[2]>nums[1]) && (nums[1]+nums[2]>nums[0])){
+        if(nums[0]==nums[1] && nums[1]==nums[2]){
+            return "equilateral";
+        }else if(nums[0]!=nums[1] && nums[1]!=nums[2] && nums[0]!=nums[2]){
+            return "scalene";
+        } else{
+            return "isosceles";
+        }
+        }
+        return "none";
+    }
+}
